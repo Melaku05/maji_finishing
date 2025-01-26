b1020588ae804a2403460dd248165a24f35ee26e
Elsabet <elsabetshiferaw1611199@gmail.com>
2025-01-26 16:48:07 +0300

    finalize the project

--- contact/admin.py
+++ contact/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import ContactMessage
+from django.utils.html import format_html
 from msigana_ecommerce.admin_site import admin_site
 
 class ContactMessageAdmin(admin.ModelAdmin):
@@ -7,5 +8,22 @@ class ContactMessageAdmin(admin.ModelAdmin):
     readonly_fields = ('full_name', 'email', 'phone', 'message', 'created_at')
     search_fields = ('full_name', 'email', 'phone')
     list_filter = ('created_at',)
+
+
+    def email_login_link(self, obj=None):
+        return format_html(
+            '<a href="{}" target="_blank" style="color: green; font-weight: bold;">Email Login</a>',
+            'https://sonaplc.com:2096/webmaillogout.cgi/'
+        )
+    email_login_link.short_description = 'Email Login'  # Display name in admin
+
+    def changelist_view(self, request, extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['email_login_link'] = self.email_login_link()  # Pass the link to template context
+        return super().changelist_view(request, extra_context=extra_context)
+
+    # Optionally, add the link to the admin page as a readonly field
+    readonly_fields = ('email_login_link',)
+
 
 admin_site.register(ContactMessage, ContactMessageAdmin)
